fb1ab91d544e64c2303cc2e29b8838f674354df2
Lucas G. Costa <lucasg.exe@gmail.com>
2025-01-25 13:26:24 -0300

    chore: configuração inicial do projeto em Java e criação do layout LoginActivity

---
+++ BTZMobileApp/app/src/main/java/com/example/btzmobileapp/view/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.btzmobileapp.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.btzmobileapp.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
